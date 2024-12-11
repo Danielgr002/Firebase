9905f01fedacd0e21cbf3021383372cc1f77980d
Danielgr002 <dangonric@alu.edu.gva.es>
2024-12-11 13:57:23 +0100

    guardado

--- app/src/main/java/com/example/firebase/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/firebase/ui/home/HomeFragment.kt
@@ -1,12 +1,18 @@
 package com.example.firebase.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+
 import com.example.firebase.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,11 +34,23 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.localitzacio
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
+
+        fun OnClickListener(){
+
+        }
+    }
+
+    private fun getLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(requireContext(), "Request permisssions", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
