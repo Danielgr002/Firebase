0e6bf0a312d740e5cd5677e5c92d0de7e245babb
Danielgr002 <dangonric@alu.edu.gva.es>
2024-12-12 13:45:52 +0100

    guardado

--- app/src/main/java/com/example/firebase/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/firebase/ui/home/HomeFragment.kt
@@ -5,10 +5,11 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -22,7 +23,7 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private val locationPermissionRequest:  ActivityResultLauncher<String>? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,9 +40,26 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+        val locationPermissionRequest =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                val fineLocationGranted: Boolean = permissions.getOrDefault(
+                    Manifest.permission.ACCESS_FINE_LOCATION, false
+                )
+                val coarseLocationGranted: Boolean = permissions.getOrDefault(
+                    Manifest.permission.ACCESS_COARSE_LOCATION, false
+                )
+                if (fineLocationGranted != null && fineLocationGranted) {
+                    getLocation();
+                } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                    getLocation()
+                } else {
+                    Toast.makeText(requireContext(), "No concedeixen permisos", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
 
-        fun OnClickListener(){
-
+        fun OnClickListener() {
+            getLocation()
         }
     }
 
@@ -50,6 +68,12 @@ class HomeFragment : Fragment() {
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED){
             Toast.makeText(requireContext(), "Request permisssions", Toast.LENGTH_SHORT).show()
+            locationPermissionRequest?.launch( arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ).toString())
+        } else {
+            Toast.makeText(requireContext(), "getLocation: permission granted", Toast.LENGTH_SHORT).show()
         }
     }
 
