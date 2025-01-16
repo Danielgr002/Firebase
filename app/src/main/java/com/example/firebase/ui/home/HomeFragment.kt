abd34fd53f6926a4a84591b24861b3875970dced
Danielgr002 <dangonric@alu.edu.gva.es>
2025-01-16 13:22:53 +0100

    guardado

--- app/src/main/java/com/example/firebase/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/firebase/ui/home/HomeFragment.kt
@@ -1,151 +1,172 @@
 package com.example.firebase.ui.home
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
-import android.location.Location
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.service.controls.ControlsProviderService.TAG
-import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ProgressBar
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.firebase.databinding.FragmentHomeBinding
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import java.io.IOException
-import java.util.Locale
-import java.util.concurrent.Executor
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import com.google.android.gms.location.LocationCallback
 
 
 class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-    private val locationPermissionRequest:  ActivityResultLauncher<String>? = null
-    private var mLastLocation: Location? = null
+    private var binding: FragmentHomeBinding? = null
+    private var locationPermissionRequest: ActivityResultLauncher<Array<String>>? = null
     private var mFusedLocationClient: FusedLocationProviderClient? = null
+    private var mTrackingLocation = false
+    private var mLocationCallback: LocationCallback? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    ): View? {
+        val homeViewModel = ViewModelProvider(this).get(
+            HomeViewModel::class.java
+        )
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding!!.root
 
-        val textView: TextView = binding.localitzacio
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+
+        SharedViewModel.currentAddress.observe(
+            viewLifecycleOwner,
+            Observer<String> { address: String? ->
+                binding!!.localitzacio.text = String.format(
+                    "Direcció: %1\$s \n Hora: %2\$tr",
+                    address, System.currentTimeMillis()
+                )
+            })
+        sharedViewModel.getButtonText().observe(viewLifecycleOwner) { s ->
+            binding!!.buttonLocation.setText(
+
+            )
         }
-        val locationPermissionRequest =
-            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-                val fineLocationGranted: Boolean = permissions.getOrDefault(
-                    Manifest.permission.ACCESS_FINE_LOCATION, false
-                )
-                val coarseLocationGranted: Boolean = permissions.getOrDefault(
-                    Manifest.permission.ACCESS_COARSE_LOCATION, false
-                )
-                if (fineLocationGranted != null && fineLocationGranted) {
-                    getLocation()
-                } else if (coarseLocationGranted != null && coarseLocationGranted) {
-                    getLocation()
-                } else {
-                    Toast.makeText(requireContext(), "No concedeixen permisos", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
+        sharedViewModel.getProgressBar().observe(viewLifecycleOwner) { visible ->
+            if (visible) binding!!.loading.visibility = ProgressBar.VISIBLE
+            else binding!!.loading.visibility = ProgressBar.INVISIBLE
+        }
+
+
+        binding!!.buttonLocation.setOnClickListener { view ->
+            Log.d("DEBUG", "Clicked Get Location")
+            sharedViewModel.switchTrackingLocation();
+        }
+
         return root
     }
 
-    private fun getLocation() {
-        if (ContextCompat.checkSelfPermission(requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
-            Toast.makeText(requireContext(), "Request permisssions", Toast.LENGTH_SHORT).show()
-            locationPermissionRequest?.launch( arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ).toString())
-        } else {
-            mFusedLocationClient!!.lastLocation.addOnSuccessListener {
-                location ->
-                if (location != null) {
-                    fetchAddress(location)
-                } else {
-                    binding.localitzacio.text = "Sense localització coneguda"
-                }
-            }
-        }
-        binding.localitzacio.setText("Carregant...")
-    }
+//    private fun startTrackingLocation() {
+//        if (ContextCompat.checkSelfPermission(
+//                requireContext(),
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            Toast.makeText(requireContext(), "Request permisssions", Toast.LENGTH_SHORT).show()
+//            locationPermissionRequest!!.launch(
+//                arrayOf(
+//                    Manifest.permission.ACCESS_FINE_LOCATION,
+//                    Manifest.permission.ACCESS_COARSE_LOCATION
+//                )
+//            )
+//        } else {
+//            Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT)
+//                .show()
+//            mFusedLocationClient!!.requestLocationUpdates(
+//                locationRequest,
+//                mLocationCallback!!, null
+//            )
+//        }
+//        binding!!.localitzacio.text = "Carregant..."
+//        binding!!.loading.setVisibility(ProgressBar.VISIBLE)
+//        mTrackingLocation = true
+//        binding!!.buttonLocation.text = "Aturar el seguiment de la ubicació"
+//    }
 
-    fun fetchAddress( location: Location){
-        var executor : ExecutorService = Executors.newSingleThreadExecutor()
-        var handler : Handler = Handler(Looper.getMainLooper())
+//    private val locationRequest: LocationRequest
+//        get() {
+//            val locationRequest: LocationRequest = LocationRequest()
+//            locationRequest.setInterval(10000)
+//            locationRequest.setFastestInterval(5000)
+//            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+//            return locationRequest
+//        }
+//
+//    private fun stopTrackingLocation() {
+//        if (mTrackingLocation) {
+//            binding?.loading?.setVisibility(ProgressBar.INVISIBLE)
+//            mTrackingLocation = false
+//            binding!!.buttonLocation.text = "Comença a seguir la ubicació"
+//            mFusedLocationClient!!.removeLocationUpdates(mLocationCallback!!)
+//        }
+//    }
 
-        var geocoder : Geocoder = Geocoder(requireContext(), Locale.getDefault())
-        executor.execute {
-          var addresses : List<Address>? = null
-          var resultMessage: String = ""
-
-          try {
-              addresses = geocoder.getFromLocation(
-                  location.latitude,
-                  location.longitude,
-                  1
-              )
-
-              if (addresses == null || addresses.size == 0){
-                  if (resultMessage.isEmpty()) {
-                      resultMessage = "No se ha encontrado ninguna ubicacion"
-                      Log.e("INCISIVE" , resultMessage)
-                  }
-              } else {
-                  var address : Address = addresses.get(0)
-                  var addressParts : ArrayList<String>? = null
-                  for (i in 0..address.maxAddressLineIndex){
-                      addressParts?.add(address.getAddressLine(i))
-                  }
-                  resultMessage = TextUtils.join("\n", addressParts!!)
-                  var finalResultManager : String = resultMessage
-                  handler.post {
-                   binding.localitzacio.setText(String.format(
-                       "Direcció: %1$s \n Hora: %2$tr",
-                       finalResultManager, System.currentTimeMillis()))
-                  }
-              }
-          }catch (e : IOException){
-              resultMessage = "Servicio no disponible"
-              Log.e("INCISIVE", resultMessage, e)
-          } catch (e : IllegalArgumentException){
-              resultMessage = "Coordenadas no validas"
-              Log.e("INCISIVE", resultMessage + ". "+ "Latitude = "+ location.latitude + ", Longitude = "+ location.longitude, e)
-          }
-        }
-    }
+//    private fun fetchAddress(location: Location?) {
+//        val executor = Executors.newSingleThreadExecutor()
+//        val handler = Handler(Looper.getMainLooper())
+//
+//        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+//
+//        executor.execute {
+//            // Aquest codi s'executa en segon pla
+//            var addresses: List<Address>? = null
+//            var resultMessage = ""
+//            try {
+//                addresses = geocoder.getFromLocation(
+//                    location!!.latitude,
+//                    location.longitude,  // En aquest cas, sols volem una única adreça:
+//                    1
+//                )
+//
+//
+//                if (addresses == null || addresses.size == 0) {
+//                    if (resultMessage.isEmpty()) {
+//                        resultMessage = "No s'ha trobat cap adreça"
+//                        Log.e("INCIVISME", resultMessage)
+//                    }
+//                } else {
+//                    val address = addresses[0]
+//                    val addressParts =
+//                        ArrayList<String?>()
+//
+//                    for (i in 0..address.maxAddressLineIndex) {
+//                        addressParts.add(address.getAddressLine(i))
+//                    }
+//
+//                    resultMessage = TextUtils.join("\n", addressParts)
+//                    val finalResultMessage = resultMessage
+//                    handler.post {
+//                        // Aquest codi s'executa en primer pla.
+//                        if (mTrackingLocation) binding!!.localitzacio.text = String.format(
+//                            "Direcció: %1\$s \n Hora: %2\$tr",
+//                            finalResultMessage,
+//                            System.currentTimeMillis()
+//                        )
+//                    }
+//                }
+//            } catch (ioException: IOException) {
+//                resultMessage = "Servei no disponible"
+//                Log.e("INCIVISME", resultMessage, ioException)
+//            } catch (illegalArgumentException: IllegalArgumentException) {
+//                resultMessage = "Coordenades no vàlides"
+//                Log.e(
+//                    "INCIVISME",
+//                    resultMessage + ". " + "Latitude = " + location!!.latitude + ", Longitude = " + location.longitude,
+//                    illegalArgumentException
+//                )
+//            }
+//        }
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
-
---
+++ app/src/main/java/com/example/firebase/ui/home/SharedViewModel.kt
@@ -0,0 +1,142 @@
+package com.example.firebase.ui.home
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Handler
+import android.os.Looper
+import android.text.TextUtils
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import java.io.IOException
+import java.util.Locale
+import java.util.concurrent.Executors
+
+
+class SharedViewModel(application: Application) : AndroidViewModel(application) {
+    private val app: Application? = null
+    val currentAddress: MutableLiveData<String> = MutableLiveData()
+    private val checkPermission = MutableLiveData<String>()
+    private val buttonText = MutableLiveData<String>()
+    private val progressBar = MutableLiveData<Boolean>()
+
+    private var mTrackingLocation : Boolean? = null
+    var mFusedLocationClient : FusedLocationProviderClient? = null
+
+    private val mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            if (locationResult != null) {
+                fetchAddress(locationResult.lastLocation)
+            }
+        }
+    }
+
+    val locationRequest: LocationRequest
+        get() {
+            val locationRequest: LocationRequest = LocationRequest()
+            locationRequest.setInterval(10000)
+            locationRequest.setFastestInterval(5000)
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            return locationRequest
+        }
+
+    fun switchTrackingLocation() {
+        if (!mTrackingLocation!!) {
+            startTrackingLocation(true)
+        } else {
+            stopTrackingLocation()
+        }
+    }
+
+    private fun stopTrackingLocation() {
+        if (mTrackingLocation!!) {
+            mFusedLocationClient?.removeLocationUpdates(mLocationCallback)
+            mTrackingLocation = false
+            progressBar.postValue(false)
+            buttonText.setValue("Comença a seguir la ubicació")
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun startTrackingLocation(needsChecking: Boolean) {
+        if (needsChecking) {
+            checkPermission.postValue("check");
+        } else {
+            mFusedLocationClient?.requestLocationUpdates(
+                    locationRequest
+                mLocationCallback, null
+            );
+
+            currentAddress.postValue("Carregant...");
+
+            progressBar.postValue(true);
+            mTrackingLocation = true;
+            buttonText.setValue("Aturar el seguiment de la ubicació");
+        }
+    }
+
+
+    private fun fetchAddress(location: Location?) {
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+        executor.execute {
+            // Aquest codi s'executa en segon pla
+            var addresses: List<Address>? = null
+            var resultMessage = ""
+            try {
+                addresses = geocoder.getFromLocation(
+                    location!!.latitude,
+                    location.longitude,  // En aquest cas, sols volem una única adreça:
+                    1
+                )
+
+
+                if (addresses == null || addresses.size == 0) {
+                    if (resultMessage.isEmpty()) {
+                        resultMessage = "No s'ha trobat cap adreça"
+                        Log.e("INCIVISME", resultMessage)
+                    }
+                } else {
+                    val address = addresses[0]
+                    val addressParts =
+                        ArrayList<String?>()
+
+                    for (i in 0..address.maxAddressLineIndex) {
+                        addressParts.add(address.getAddressLine(i))
+                    }
+
+                    resultMessage = TextUtils.join("\n", addressParts)
+                    val finalResultMessage = resultMessage
+                    handler.post {
+                        // Aquest codi s'executa en primer pla.
+                        if (mTrackingLocation) binding!!.localitzacio.text = String.format(
+                            "Direcció: %1\$s \n Hora: %2\$tr",
+                            finalResultMessage,
+                            System.currentTimeMillis()
+                        )
+                    }
+                }
+            } catch (ioException: IOException) {
+                resultMessage = "Servei no disponible"
+                Log.e("INCIVISME", resultMessage, ioException)
+            } catch (illegalArgumentException: IllegalArgumentException) {
+                resultMessage = "Coordenades no vàlides"
+                Log.e(
+                    "INCIVISME",
+                    resultMessage + ". " + "Latitude = " + location!!.latitude + ", Longitude = " + location.longitude,
+                    illegalArgumentException
+                )
+            }
+        }
+    }
+}
